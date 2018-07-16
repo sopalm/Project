4f1f46b2fcabe9322fb0ddbf2b4e1047c2770164
Samssd <densirimongkol@hotmail.com>
2018-07-16 15:54:54 +0700

    gae bug info

--- androidPHP2/app_info.php
+++ androidPHP2/app_info.php
@@ -16,10 +16,9 @@
     ) or die("Failed db".mysqli_error());
     $row = mysqli_fetch_array($result);	 
     if($row['emp_id']&&$row['csd_no']){
-        $sqlCommand = "UPDATE `personal_information` SET `weight`='$weight',`height`='$height',`bmi`='$bmi',`blood_pressure`='$bp',`blood_pressure_extra`='$bp2',`date_modify`='$_SESSION[date]',`user`='$user' WHERE emp_id = '$emp_id' AND csd_no = '$csd_no')";
+        $sqlCommand = "UPDATE `personal_information` SET `weight`='$weight',`height`='$height',`bmi`='$bmi',`blood_pressure`='$bp',`blood_pressure_extra`='$bp2',`date_modify`='$_SESSION[date]',`user`='$user' WHERE emp_id = '$emp_id' AND csd_no = '$csd_no' ";
             $result=mysqli_query($con,$sqlCommand)
                 or die("Failed db".mysqli_error());
-                
     }  
     else{
         $sqlCommand2 = "INSERT INTO `personal_information`(`pi_id`,`emp_id`, `csd_no`, `weight`, `height`, `bmi`, `blood_pressure`, `blood_pressure_extra`, `date_modify`, `user`) VALUES (NUll,'$emp_id','$csd_no','$weight','$height','$bmi','$bp','$bp2','$_SESSION[date]','$user')";
