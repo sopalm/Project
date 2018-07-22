b5cc545bd29585b73abbcc60a6d6c30c84fdefed
SamSam <songdech20619@gmail.com>
2018-07-22 23:39:55 +0700

    edit function check_id

--- androidPHP2/app_family.php
+++ androidPHP2/app_family.php
@@ -18,13 +18,13 @@
     $row = mysqli_fetch_array($result);	 
     if($row['emp_id']&&$row['csd_no']){
         $sqlCommand = "UPDATE `personal_family_information` SET`heart`= '$x1',`hypertension`= '$x2',`dyslipidemia`= '$x3',`diabetes_mellitus`= '$x4',`cancer`= '$x5',`date_modify`='$_SESSION[date]',`user_modify`='$user' WHERE emp_id = '$emp_id' AND csd_no = '$csd_no' ";
-            $result=mysqli_query($con,$sqlCommand)
-                or die("Failed db".mysqli_error());
+        mysqli_query($con,$sqlCommand);
+                echo $sqlCommand;
     }  
     else{
-        $sqlCommand2 = "INSERT INTO `personal_family_information`(`pfi_id`, `emp_id`, `csd_no`, `heart`, `hypertension`, `dyslipidemia`, `diabetes_mellitus`, `cancer`, `date_modify`, `user_modify`) VALUES (NULL,'$emp_id','$csd_no','$x1','$x2,'$x3','$x4','$x5','$_SESSION[date]','$user')";
-            $result=mysqli_query($con,$sqlCommand2)
-                or die("Failed db".mysqli_error());
+        $sqlCommand2 = "INSERT INTO `personal_family_information`(`pfi_id`, `emp_id`, `csd_no`, `heart`, `hypertension`, `dyslipidemia`, `diabetes_mellitus`, `cancer`, `date_modify`, `user_modify`) VALUES (NULL,'$emp_id','$csd_no','$x1','$x2','$x3','$x4','$x5','$_SESSION[date]','$user')";
+        mysqli_query($con,$sqlCommand2);
+                echo $sqlCommand2;
     }
 	
 ?>
